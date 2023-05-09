cc8263751144021bffba2727eff6e4617364397b
Loi Tran <datloiboi@gmail.com>
2023-05-09 08:37:18 -0400

    Initial commit

---
+++ com.primetimetran.swift-hello/com_primetimetran_swift_helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  com_primetimetran_swift_helloApp.swift
+//  com.primetimetran.swift-hello
+//
+//  Created by Loi Tran on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct com_primetimetran_swift_helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
